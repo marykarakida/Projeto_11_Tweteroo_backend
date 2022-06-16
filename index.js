93c4b277c20350015464643b4df7bde641797525
Mary Karakida <makarakida@gmail.com>
2022-06-15 22:32:49 -0300

    navigate to main page after sign in

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 const app = express();
 
 let usuarios = [];
+let tweets = [];
 
 app.use(express.json())
 app.use(cors());
@@ -11,6 +12,10 @@ app.use(cors());
 app.post("/sign-up", (req, res) => {
     usuarios.push(req.body);
     res.send("ok")
+})
+
+app.get("/tweets", (req, res) => {
+    res.send(tweets);
 })
 
 
