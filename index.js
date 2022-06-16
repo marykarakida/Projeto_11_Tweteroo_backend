33dbf541939039127abe4d6d9eb252ed8fcbe08e
Mary Karakida <makarakida@gmail.com>
2022-06-15 22:48:59 -0300

    add tweet post function

--- index.js
+++ index.js
@@ -11,11 +11,26 @@ app.use(cors());
 
 app.post("/sign-up", (req, res) => {
     usuarios.push(req.body);
-    res.send("ok")
+    res.send("OK")
 })
 
 app.get("/tweets", (req, res) => {
     res.send(tweets);
+})
+
+app.post("/tweets", (req, res) => {
+    const tweet = req.body;
+
+    for (let i = 0 ; i < usuarios.length ; i ++) {
+        const user = usuarios[i];
+        if (user.username === tweet.username) {
+            tweet.avatar = user.avatar;
+            break;
+        }
+    }
+
+    tweets.push(tweet);
+    res.send("OK");
 })
 
 
